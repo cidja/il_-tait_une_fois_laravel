3edaf9eeb52620b489182a5cbf4e6c22b0762af6
cidja <chgeorgesc@gmail.com>
2020-12-07 21:04:43 +0100

    update welcome.blade with tailwind css

--- resources/views/pages/frontend/welcome.blade.php
+++ resources/views/pages/frontend/welcome.blade.php
@@ -10,16 +10,16 @@
   <!--Formulaire de recherche en haut de la page !-->
   <section>
     <form class="" action="search" method="get">
-      <input type="text" name="search" placeholder="rentrez votre recherche"/>
-      <input type="submit" name="submitSearch" value="valider" />
+      <input class="border-2 border-solid border-indigo-500 rounded-md py-1 px-2" type="text" name="search" placeholder="rentrez votre recherche"/>
+      <input class="bg-indigo-400 py-2 px-8 rounded-full" type="submit" name="submitSearch" value="valider" />
     </form>
   </section>
 
   <section>
-    <h2>Affichage des 10 derniers romans</h2>
+    <h2 class="text-center my-5 text-lg">Affichage des 10 derniers romans</h2>
     <div class="container mx-auto">
       @foreach($novels as $novel)
-        <div class="container allnovel mb-4">
+        <div class="container allnovel mb-4 py-10 bg-gradient-to-r from-green-400 to-blue-500 rounded-md ">
           <div class="cover w-40 mx-auto">
 
           @php //to display a php 
@@ -42,7 +42,9 @@
             <div>{{ $novel->author }} </div>
           </div>
           <div class="more flex flex-row justify-center">
-            <a class="bg-green-300 p-2 rounded-md text-gray-50 hover:text-black" href="{{  route('singleNovel', [$novel->id])  }}">en savoir plus</a>
+            <a class="bg-green-600 p-2 rounded-md text-gray-50 hover:text-black" href="{{  route('singleNovel', [$novel->id])  }}">
+              en savoir plus
+            </a>
           </div>
           
 
